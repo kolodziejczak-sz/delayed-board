1b9c81e4403ba73b1e82ba178c9f2763cbf5ba54
szymon <s.kolodziejczak95@gmail.com>
2019-06-29 22:12:22 +0200

    ramda import fix

--- src/redux/users.js
+++ src/redux/users.js
@@ -2,17 +2,21 @@ import * as R from 'ramda';
 import Immutable from 'seamless-immutable';
 import { createReducers, createActions } from 'redux-arc';
 import { createUser } from '../models/user';
+import icons from '../constants/icons';
 
 export const { types, creators } = createActions('users', {
   updateUser: null,
 });
 
-const initialState = Immutable([createUser('User 1', 0), createUser('User 2', 1)]);
+const initialState = Immutable([
+  createUser('User 1', icons[0]),
+  createUser('User 2', icons[1]),
+]);
 
 const onUpdateUser = (state, action) => {
   const update = action.payload;
 
-  return R.map(when(R.propEq('id', update.id), R.mergeRight(R.__, update)), state);
+  return R.map(R.when(R.propEq('id', update.id), R.mergeRight(R.__, update)), state);
 };
 
 export default createReducers(initialState, {
